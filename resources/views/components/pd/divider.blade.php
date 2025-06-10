ee2ed008ca5c1288060e5b295d0ca1b59f772f57
Jesus Alfonzo <jesusalfonzo.code@gmail.com>
2025-06-10 11:36:19 -0400

    Primer commit

---
+++ resources/views/components/pd/divider.blade.php
@@ -0,0 +1,5 @@
+<div class="flex w-full items-center rounded-full">
+<div class="flex-1 border-b border-gray-300"></div>
+<span class="text-black text-lg font-semibold leading-8 px-8 py-3">Bienvenido {{ auth()->user()->name }}</span>
+<div class="flex-1 border-b border-gray-300"></div>
+</div>
---
+++ resources/views/components/pd/stat.blade.php
@@ -0,0 +1,46 @@
+<section class="py-8">
+  <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+    <div class="flex flex-col flex-1 gap-10 lg:gap-0 lg:flex-row lg:justify-between">
+      <!-- Añade dark:border-gray-800 a todos los bordes -->
+      <div class="w-full lg:w-1/4 border-b pb-10 lg:border-b-0 lg:pb-0 lg:border-r border-gray-100 dark:border-gray-800">
+        <!-- Añade dark:text-white a textos principales -->
+        <div class="font-manrope font-bold text-5xl text-gray-900 dark:text-white mb-5 text-center">
+          260+
+        </div>
+        <!-- Añade dark:text-gray-300 a textos secundarios -->
+        <span class="text-xl text-gray-500 dark:text-gray-300 text-center block">
+          Expert Consultants
+        </span>
+      </div>
+      
+      <!-- Repite para cada bloque -->
+      <div class="w-full lg:w-1/4 border-b pb-10 lg:border-b-0 lg:pb-0 lg:border-r border-gray-100 dark:border-gray-800">
+        <div class="font-manrope font-bold text-5xl text-gray-900 dark:text-white mb-5 text-center">
+          975+
+        </div>
+        <span class="text-xl text-gray-500 dark:text-gray-300 text-center block">
+          Active Clients
+        </span>
+      </div>
+      
+      <div class="w-full lg:w-1/4 border-b pb-10 lg:border-b-0 lg:pb-0 lg:border-r border-gray-100 dark:border-gray-800">
+        <div class="font-manrope font-bold text-5xl text-gray-900 dark:text-white mb-5 text-center">
+          724+
+        </div>
+        <span class="text-xl text-gray-500 dark:text-gray-300 text-center block">
+          Projects Delivered
+        </span>
+      </div>
+      
+      <div class="w-full lg:w-1/4">
+        <div class="font-manrope font-bold text-5xl text-gray-900 dark:text-white mb-5 text-center">
+          89+
+        </div>
+        <span class="text-xl text-gray-500 dark:text-gray-300 text-center block">
+          Orders in Queue
+        </span>
+      </div>
+    </div>
+  </div>
+</section>
+
